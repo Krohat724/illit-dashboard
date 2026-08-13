52454a07388137abe68e365e6f671febd390ad1e
Krohat724 <kuraoka.works@gmail.com>
2026-08-13 21:40:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ supabase_headers = {
 DEFAULT_CONCEPTS = {
     "Vk5-c_v4gMU": "イージーリスニング",
     "a81S40mS_4A": "Y2K・レトロ",
-    "43MlyGCoQ7k": "ガールクラッシュ
+    "43MlyGCoQ7k": "ガールクラッシュ"
 }
 
 CONCEPT_KEYWORDS = {
