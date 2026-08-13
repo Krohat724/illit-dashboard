2acc1c55026028c85dfdfa8465be8eb43c2ee5d1
Krohat724 <kuraoka.works@gmail.com>
2026-08-13 22:07:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ supabase_headers = {
 CONCEPT_KEYWORDS = {
     "イージーリスニング": [
         "easy listening", "chill", "lo-fi", "アコースティック", "イージーリスニング", 
-        "magnetic", "illit", "아일릿", "cherish", "tick-tack", "lucky girl", "iykyk", "lofi", "soft"
+        "magnetic", "아일릿", "cherish", "tick-tack",  "iykyk", "lofi", "soft"
     ],
     "Y2K・レトロ": [
         "y2k", "retro", "レトロ", "nostalgia", "90s", "00s", "newjeans", "뉴진스", 
